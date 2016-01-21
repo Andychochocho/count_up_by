e56b98926ff898fd6afc39589dd787102fc3cadd
Andrew Cho and Matt Rosanio <cho-andrew@hotmail.com>
2016-01-21 10:57:44 -0800

    finalize project with user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 function countByInterval(countTo, countBy) {
   if (countBy === undefined || countTo === undefined) {
-    // alert("Please fill out all values");
+    alert("Please fill out all values");
     return false;
   } else if (countBy === 0){
-    // alert("Can't use 0 for increment value")
+    alert("Can't use 0 for increment value")
     return false;
   } else if (isNaN(countTo) || isNaN(countBy)) {
-    // alert("Please enter numbers only");
+    alert("Please enter numbers only");
     return false;
   }
   var array = [];
   if (countTo <= 0) {
     if (countBy >= 0) {
+      alert("Your values are incompatible");
       return false;
     }
     for (var i = countBy; i >= countTo; i+= countBy) {
@@ -19,6 +20,7 @@ function countByInterval(countTo, countBy) {
     }
   } else {
     if (countBy <= 0) {
+      alert("Your values are incompatible");
       return false;
     }
     for (var i = countBy; i <= countTo; i+= countBy) {
@@ -27,3 +29,16 @@ function countByInterval(countTo, countBy) {
   }
   return array;
 }
+
+$(function(){
+  $("form#countUp").submit(function(event){
+    var countTo = parseInt($("#countTo").val());
+    var countBy = parseInt($("#countBy").val());
+    var result = countByInterval(countTo,countBy);
+    for (var i = 0; i < result.length; i++) {
+      $("#result").append(result[i] + ", ");
+    }
+    $('#result').show();
+    event.preventDefault();
+  });
+});
