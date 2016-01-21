511acda6a72b9888f665c7e00685a40c759e1559
Andrew Cho and Matt Rosanio <mmrosani@syr.edu>
2016-01-21 09:27:38 -0800

    alert user when second arguement is 0

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ function countByInterval(countTo, countBy) {
   if (countBy === undefined || countTo === undefined) {
     alert("Please fill out all values");
     return false;
+  } else if (countBy === 0){
+    alert("Can't use 0 for increment value")
+    return false;
   }
   var array = [];
   for (var i = countBy; i <= countTo; i+= countBy) {
--- spec/specs.js
+++ spec/specs.js
@@ -5,6 +5,9 @@ describe ('countByInterval', function() {
   it("will count up to designated number in a specialized interval", function(){
     expect(countByInterval(12,3)).to.eql([3,6,9,12]);
   });
+  it("will return false when second arguement is 0",function(){
+    expect(countByInterval(17,0)).to.eql(false);
+  });
   it("will return false when it has empty input", function(){
     expect(countByInterval(5)).to.eql(false);
   });
